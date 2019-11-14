8e3a71f2508613f80a26fa1e8af2625913f198f1
eejohnson1 <erik.johnson0110@gmail.com>
2019-11-14 13:41:27 -0600

    Added Test Items and Users to "create" and "purge" SQL files

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -16,7 +16,7 @@ CREATE TABLE borrow_item (
     id INT NOT NULL AUTO_INCREMENT,
 	item_id INT NOT NULL,
 	lender_id INT NOT NULL,
-	borrower_id INT NOT NULL,
+	borrower_id INT,
 	borrow_date DATE,
 	due_date DATE,
 	return_date DATE,
@@ -35,3 +35,23 @@ CREATE TABLE borrow_rating (
     FOREIGN KEY(lender_id) REFERENCES user(id),
 	FOREIGN KEY(borrower_id) REFERENCES user(id)
 );
+
+INSERT INTO user (username, password)
+VALUES ('james_bond','007');
+INSERT INTO user (username, password)
+VALUES ('SpongeRobertPants','1234');
+INSERT INTO user (username, password)
+VALUES ('tokoyo_sexwhistle','abcd');
+
+INSERT INTO item (name, type)
+VALUES ('Dune', 'Book');
+INSERT INTO item (name, type)
+VALUES ('Nintendo Switch', 'Game System');
+INSERT INTO item (name, type)
+VALUES ('Sweet Shirt', 'Shirt');
+INSERT INTO item (name, type)
+VALUES ('Underpants', 'Clothes');
+INSERT INTO item (name, type)
+VALUES ('Toothbrush', 'Utensil');
+INSERT INTO item (name, type)
+VALUES ('Hopes and Dreams', 'Abstract Concept');
